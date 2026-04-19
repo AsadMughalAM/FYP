48ad6b5745bd9bf817701c815ff475e4693d1f27
AsadMughal <am127955@gmail.com>
2026-04-19 22:30:47 +0500

    Fix: add custom CORS middleware for Railway/Vercel

---
+++ server/server/middleware.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+
+
+class ForceCorsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        
+        # Always add CORS headers
+        origin = request.headers.get('Origin', '*')
+        response['Access-Control-Allow-Origin'] = origin
+        response['Access-Control-Allow-Credentials'] = 'true'
+        response['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+        response['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, X-Requested-With'
+        
+        return response
